64a9650d2ef081d1c6224eb47499683e1b238c29
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-10-11 22:01:23 +0000

    add nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,12 +6,27 @@ const Nav = () => {
     <>
       <div className="w-full bg-blue-500 p-8">
         <nav className="flex justify-center gap-20">
-          <Link to="#" className="">
+          <Link
+            to="#"
+            className="text-center font-medium bg-white p-2 w-25 rounded-sm hover:bg-blue-800 hover:text-white "
+          >
             {" "}
             Home{" "}
           </Link>
-          <Link to="#"> Add task </Link>
-          <Link to="#"> Task List </Link>
+          <Link
+            to="#"
+            className="text-center font-medium bg-white p-2 w-25 rounded-sm hover:bg-blue-800 hover:text-white "
+          >
+            {" "}
+            Add task{" "}
+          </Link>
+          <Link
+            to="#"
+            className="text-center font-medium bg-white p-2 w-25 rounded-sm hover:bg-blue-800 hover:text-white "
+          >
+            {" "}
+            Task List{" "}
+          </Link>
         </nav>
       </div>
     </>
