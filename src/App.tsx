ba0386de764ad993af03a00319ce885bdd5bc07f
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-10-11 22:09:08 +0000

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { TaskList } from "./pages/TaskList";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
-
+import AddTask from "./pages/AddTask";
 const route = createBrowserRouter([
   // default page
   {
@@ -11,6 +11,7 @@ const route = createBrowserRouter([
     children: [
       { index: true, Component: HomePage },
       { path: "tasks", Component: TaskList },
+      { path: "add-task", Component: AddTask },
     ],
   },
 ]);
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import AddTask from "../pages/AddTask";
 const Nav = () => {
   return (
     <>
       <div className="w-full bg-blue-500 p-8">
         <nav className="flex justify-center gap-20">
           <Link
-            to="#"
+            to="/"
             className="text-center font-medium bg-white p-2 w-25 rounded-sm hover:bg-blue-800 hover:text-white "
           >
             {" "}
             Home{" "}
           </Link>
           <Link
-            to="#"
+            to="add-task"
             className="text-center font-medium bg-white p-2 w-25 rounded-sm hover:bg-blue-800 hover:text-white "
           >
             {" "}
             Add task{" "}
           </Link>
           <Link
-            to="#"
+            to="tasks"
             className="text-center font-medium bg-white p-2 w-25 rounded-sm hover:bg-blue-800 hover:text-white "
           >
             {" "}
